0f9eed2e54d3c25e617c25a28cd386640bb730c2
Dongkwan Kim <todoaskit@gmail.com>
2022-03-25 13:29:31 +0900

    Init node-transfer repo

---
+++ install.sh
@@ -0,0 +1,5 @@
+pip3 install torch==1.10
+pip3 install torch-scatter==2.0.9 -f https://data.pyg.org/whl/torch-1.10.0+cu102.html
+pip3 install torch-sparse==0.6.13 -f https://data.pyg.org/whl/torch-1.10.0+cu102.html
+pip3 install torch-cluster==1.6.0 -f https://data.pyg.org/whl/torch-1.10.0+cu102.html
+pip3 install torch-geometric==2.0.4
